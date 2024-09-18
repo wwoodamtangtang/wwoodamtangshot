3e0214b060b430165fbee5d2af9a7e2146e6baab
sbock <mail@sbock.dev>
2024-09-18 21:43:14 +0200

    fix site and base config for github pages
    https://docs.astro.build/en/guides/deploy/github/#deploying-to-a-githubio-url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://0xsbock.github.io',
+  base: 'astro_gallery_template',
   integrations: [tailwind()]
 });

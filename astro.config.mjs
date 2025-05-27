0f14d191acd5191ebff01a554fa0397b6f092a47
김담우 <damwwoo@gmail.com>
2025-05-28 02:08:21 +0900

    chore: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wwoodamtangshot-git-main-damwoos-projects.vercel.app",
+  publicDir: "src/content",
   base: "/",
   integrations: [tailwind()],
 });

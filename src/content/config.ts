b2ef4019d4741e4e2401accbb365ed722564b722
김담우 <damwoo-mac@damwoos-MacBook-Pro.local>
2025-05-28 00:56:11 +0900

    chore: update

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,11 @@ import { defineCollection, z } from "astro:content";
 
 const albums = defineCollection({
   type: "data",
-  schema: () =>
-    z.object({
-      title: z.string(),
-      description: z.string().optional(),
-      cover: z.string(), // ✅ 문자열 경로로 바꿔야 src/content 내부 이미지 사용 가능
-    }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    cover: z.string(),
+  }),
 });
 
 export const collections = {
